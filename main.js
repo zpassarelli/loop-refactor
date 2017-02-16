18d4d6bef2516e0db250436da586cde55d23dc23
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2017-02-16 10:35:27 -0700

    functions written with loops

--- main.js
+++ main.js
@@ -1,23 +1,72 @@
 'use strict';
 
 module.exports = {
-  map: () => {
 
+  //take an array of arrays with firstname, lastname elements and transform each into an object with firstname, lastname key;
+  map: (arr) => {
+    let mapped = [];
+    for(var i = 0; i < arr.length; i++){
+      let obj = {};
+      obj.first = arr[i][0];
+      obj.last = arr[i][1];
+      mapped.push(obj);
+    }
+    return mapped;
   },
 
-  filter: () => {
 
+  //filter elements in an array that match search criteria
+  filter: (arr, str) => {
+    let filtered = [];
+    for(var i = 0; i < arr.length; i++){
+      if (arr[i].includes(str)){
+        filtered.push(arr[i]);
+      }
+    }
+    return filtered;
   },
 
-  reduce: () => {
-
+  reduce: (arr, base) => {
+    for (var i = 0; i < arr.length; i++){
+      base += arr[i];
+    }
+    return base;
   },
 
-  every: () => {
-
+  //checks whether each object in an array of objects contains a 'middle' property
+  every: (arr, prop) => {
+    for (var i = 0; i < arr.length; i++){
+      if(!arr[i].hasOwnProperty(prop)){
+        return false;
+      }
+    }
+    return true;
   },
 
-  some: () => {
-
-  },
+  //checks where some objects in an array contain a 'middle' property
+  some: (arr, prop) => {
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i].hasOwnProperty(prop)){
+        return true;
+      }
+    }
+    return false;
+  }
 };
+
+
+
+// let arr = [
+//   {first: 'Kristen', middle: 'Lee', last: 'Foster-Marks'},
+//   {first: 'Aaron', last: 'Marks'}
+// ];
+// console.log(module.exports.every(arr, 'first'));
+
+// let arr = [1, 2, 3, 4];
+// console.log(module.exports.reduce(arr, 0));
+
+// let arr = ['apples', 'bananas', 'oranges', 'grapes', 'watermelon'];
+// console.log(module.exports.filter(arr, 'an'));
+
+// let arr = [['Kristen', 'Foster'], ['Aaron', 'Marks']];
+// console.log(module.exports.map(arr));
